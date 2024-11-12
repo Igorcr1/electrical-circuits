2930505ca04ef056248fc399e37d895c041324c1
Igorcr1 <48606815+Igorcr1@users.noreply.github.com>
2024-11-11 23:14:48 -0300

    Update circuit_draw.py

--- src/circuit_draw.py
+++ src/circuit_draw.py
@@ -1,59 +1,91 @@
 import schemdraw
 import schemdraw.elements as elm
 
-def desenhar_circuito(elementos):
+def desenhar_circuito(nos, elementos):
+    #Vamos definir para somente 5 nós.
+    match len(nos):
+        case 2:
+            d, posicoes = basico(nos,elementos,index=2)
+            d.draw()
+        case 3:
+            d, posicoes = basico(nos,elementos,index=3)
+            d,posicoes = sec_basico(nos,elementos,d,posicoes)
+            d.draw()
+        case 4:
+            d, posicoes = basico(nos,elementos,index=4)
+            d.draw()
+        case 5:
+            d, posicoes = basico(nos,elementos,index=5)
+            pass
+
+
+def basico(nos,elementos,index=2):
+    # fonte de tensao/corrente, resistor, o ground e fecha
+
+    terra = nos[-1]
+    posicoes = {}
+    
     d = schemdraw.Drawing()
-    novo_elementos = {}
-    numero_de_elementos = len(elementos) #Esse é o ultimo
-    i = 0 #Contador para fechar o circuito, falta QI pra fazer algo normal
-    for elem, valor in elementos.items():
-        x, y, tipo, orientacao = elem
-        if(i==0): #O primeiro elemento precisa ter uma variavel diferente pra fechar o circuito.
-            a = tipagem_do_elemento(tipo,valor,orientacao)
-            d += a
-            novo_elementos[(d.here[0],d.here[1], tipo, orientacao)] = valor
-        else:
-            c = tipagem_do_elemento(tipo,valor,orientacao)
-            d += c
-            novo_elementos[(d.here[0],d.here[1], tipo, orientacao)] = valor
-        i += 1
-        if (numero_de_elementos == i):
-            fech = fechando_circuito(elementos,a)
-            d += fech
-            novo_elementos[(d.here[0],d.here[1], tipo, orientacao)] = valor
-    elementos = novo_elementos #Aqui a coisa fica legal, o dict antigo deixa de existir e o novo tem x,y
-    d += nos(elementos)
-    d.draw()
+    for elem,valor in elementos.items():
+        x,y,tipo = elem
+        if(tipo == 'R' and x=='A' and y=='B'):
+            posicoes['B'] = (d.here)
+            d += elm.Resistor().left().label(f'{valor}')
+            posicoes['A'] = (d.here)
+        elif(tipo == 'I' and x=='A' and y=='B'):
+            posicoes['A'] = (d.here)
+            d += elm.Line().up()
+            if (index == 2):
+                d += elm.Ground().left()
+            d += elm.SourceI().right().label(f'{valor}')
+            d += elm.Line().down()
+            d += elm.Dot().label('A')
+            posicoes['B'] = (d.here)
+        elif(tipo == 'V' and x=='A' and y=='B'):
+            posicoes['A'] = (d.here)            
+            d += elm.Line().up()
+            d += elm.Ground().left()
+            d += elm.SourceV().right().label(f'{valor}')
+            d += elm.Line().down()
+            d += elm.Dot().label('A')
+            posicoes['B'] = (d.here)
+        # Se não houver,A,B vc precisa meter uma linha pra indicar A E B
+    return d,posicoes
 
 
-def tipagem_do_elemento(tipo,valor,orientacao):
-    if orientacao == 'C':
-        orit = 'up'
-    elif orientacao == 'B':
-        orit = 'down'
-    elif orientacao == 'E':
-        orit = 'left'
-    elif orientacao == 'D':
-        orit = 'right'
+def sec_basico(nos,elementos,d,posicoes):
+    for elem,valor in elementos.items():
+        x,y,tipo = elem
+        #Bloco 1
+        if(tipo == 'R' and x == 'A' and y == 'C'):
+            d += elm.Resistor().at(posicoes[f'{x}']).down().label(f'{valor}')
+            posicoes['C'] = (d.here)
+        elif(tipo == 'I' and x == 'A' and y == 'C'):
+            d += elm.Line().at(posicoes[f'{x}']).left()
+            d += elm.SourceI(reverse=orit_reverse(valor)).down().label(f'{(valor)}')
+            d += elm.Line().right()
+            posicoes['C'] = (d.here)
+        #Aqui tambem precisa de um elif 
 
-    if tipo == 'R':
-        c = elm.Resistor(d=f'{orit}').label(f"{valor} Ohms")
-    elif tipo == 'V':
-        c = elm.SourceV(d=f'{orit}').label(f"{valor} V")
-    elif tipo =='A':
-        c = elm.SourceI(d=f'{orit}').label(f"{valor} A")
-    elif tipo == 'G':
-        c = elm.Ground(d=f'{orit}')
-    elif tipo == 'L':
-        c = elm.Line(d=f'{orit}')
-    return c
+        #bloco 2
+        elif(tipo == 'R' and x == 'B' and y == 'C'):
+            d += elm.Resistor().at(posicoes[f'{x}']).down().label(f'{valor}')
+        elif(tipo == 'I' and x == 'B' and y == 'C'):
+            d += elm.Line().at(posicoes[f'{x}']).right()
+            d += elm.SourceI().down().label(f'{(valor)}')
+            d += elm.Line().left()
+            d += elm.Line().left()
+            d += elm.Ground()
+            posicoes['C'] = (d.here)
+    return d,posicoes
+    
+def terc_basico(nos,elementos,d,posicoes):
+    pass
 
-def fechando_circuito(elementos,a):
-    fech = elm.Line().tox(a.start)
-    return fech
 
+def orit_reverse(valor): # A construção de algumas imagens necessita inverter o valor, sim, é uma gambiarra
+    if valor > 0:
+        return True
+    else:
+        return False
 
-def nos(elementos):
-    n = int(input('Onde você quer colocar o nó? Os elementos do circuito aparecerão em ordem. Será possível começar a colocar')) - 1
-    element = list(elementos.keys())
-    return elm.Dot().at((element[n][0], element[n][1]))
