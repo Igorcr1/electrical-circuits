e14c2ae494925f95924dc13f2b3cce24d4f64550
Igorcr1 <48606815+Igorcr1@users.noreply.github.com>
2024-11-11 23:14:25 -0300

    Update main.py

--- src/main.py
+++ src/main.py
@@ -4,22 +4,23 @@ from circuit_draw import desenhar_circuito
 
 def main():
     # Coleta de dados do usuário
-    elementos = coletar_dados_usuario()
-    
-    # Desenho do circuito
-    desenhar_circuito(elementos)
-    #desenhar_circuito(elementos = {(0, 0, 'V', 'C'): 10.0, (0, 0, 'R', 'D'): 10.0, (0, 0, 'R', 'B'): 10.0})
-
-'''
-
+    nos, elementos = coletar_dados_usuario()
+    #print(nos,elementos)
+    '''
     # Cálculo das correntes e tensões
     try:
         tensoes_nos, correntes_elementos = calcular_correntes(nos, elementos)
     except Exception as e:
         print(f"Erro ao calcular correntes e tensões: {e}")
         return
-    
-    
+    '''
+    desenhar_circuito(nos,elementos)
+    # Desenho do circuito
+    #desenhar_circuito(['A', 'B', 'C', 'D'], {('A', 'B', 'R'): 2000.0, ('A', 'C', 'R'): 1000.0, ('A', 'D', 'V'): 10.0, ('B', 'C', 'R'): 2000.0, ('B', 'D', 'R'): 1000.0, ('C', 'D', 'R'): 2000.0})
+    #desenhar_circuito(['A', 'B'], {('A', 'B', 'R'): 10.0, ('A', 'B', 'I'): 10.0})
+    #desenhar_circuito(['A', 'B', 'C'], {('A', 'B', 'R'): 5.0, ('A', 'C', 'R'): 2.0, ('A', 'C', 'I'): 3.1, ('B', 'C', 'R'): 1.0, ('B', 'C', 'I'): -1.4})
+
+    '''
     # Exibição dos resultados
     print("\nResultados:")
     for no, tensao in tensoes_nos.items():
@@ -27,8 +28,8 @@ def main():
     for elem, corrente in correntes_elementos.items():
         no1, no2, tipo = elem
         print(f"Corrente entre {no1} e {no2} ({tipo}): {corrente:.4f} A")
-'''
+    '''
     
-
 if __name__ == "__main__":
     main()
+
